88075b10dc431d4eb1c44a57852e3ee552d3ba88
Kostya Klopov <kklopov64@gmail.com>
2023-10-25 22:10:45 +0300

    Added Entity Framework

--- Holy_locket.DAL/Program.cs
+++ Holy_locket.DAL/Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Holy_locket.DAL.Repositories;
+using Microsoft.Data.SqlClient;
+using Microsoft.IdentityModel.Protocols;
+using System.Configuration;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+              Console.WriteLine("Hello, World!");
+
+        var connection =
+      System.Configuration.ConfigurationManager.
+      ConnectionStrings["connString"].ConnectionString;
+        var db = new HolyLocketContext();
+        
+        SqlConnection con = new SqlConnection(connection);
+        con.Open();
+    }
+}
---
+++ Holy_locket.DAL/Repositories/HolyLocketContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Holy_locket.DAL.Repositories
+{
+    public class HolyLocketContext : DbContext
+    {
+        public HolyLocketContext(DbContextOptions<HolyLocketContext> options) : base(options) { }
+        public HolyLocketContext()
+        {
+            int i = 0;
+        }
+
+    }
+}
