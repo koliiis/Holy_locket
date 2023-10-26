541cdd8f598e125f871542e9e4140c10752b9f84
babenko-v <146779776+babenko-v@users.noreply.github.com>
2023-10-26 12:16:06 +0300

    Create react project (#2)
    * registration page
    
    * default react project
    
    * create-react-project-2
    
    * ignor pages and idea
    
    * ignor pages-and-idea
    
    * create-react-proj

---
+++ View/frontend/src/App.js
@@ -0,0 +1,17 @@
+// import Registration from './pages/Registration';
+import './App.css';
+
+function App() {
+  return (
+      <div className="App">
+        <header>
+          <h1 className="main">Добро пожаловать на мой сайт!</h1>
+        </header>
+        <main>
+          <Registration/>
+        </main>
+      </div>
+  );
+}
+
+export default App;
