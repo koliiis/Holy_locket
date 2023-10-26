f96d405dd2d1f4861384833067ea07d93282c935
Kostya Klopov <kklopov64@gmail.com>
2023-10-26 21:40:24 +0300

    temp

--- Holy_locket.DAL/Models/Hostital.cs
+++ Holy_locket.DAL/Models/Hostital.cs
@@ -8,23 +8,21 @@ using System.Threading.Tasks;
 
 namespace Holy_locket.DAL.Models
 {
-    internal class Hostital : TEntity
+    public class Hostital : TEntity
     {
         public override int Id { get; set; }
         [Required]
         [MaxLength(15)]
+        [MinLength(4)]
         public string Title { get; set; }
         [Required]
-        [MaxLength(100)]
-        [MinLength(4)]
+        [MaxLength(20)]
+        [MinLength(7)]
         public string Phone { get; set; }
         [Required]
         [MaxLength(20)]
         [MinLength(7)]
         public string Adress { get; set; }
-        [Required]
-        [MaxLength(50)]
-        [MinLength(7)]
-
     }
+
 }
--- Holy_locket.DAL/Models/Patient.cs
+++ Holy_locket.DAL/Models/Patient.cs
@@ -1,12 +1,32 @@
-﻿using System;
+﻿using Holy_locket.DAL.Abstracts;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Holy_locket.DAL.Models
 {
-    internal class Patient
+    public class Patient : TEntity
     {
+        public override int Id { get; set; }
+        [Required]
+        [MaxLength(15)]
+        [MinLength(2)]
+        public string FirstName { get; set; }
+        [Required]
+        [MaxLength(15)]
+        [MinLength(2)]
+        public string SecondName { get; set; }
+        [Required]
+        [MaxLength(20)]
+        [MinLength(7)]
+        public string Phone { get; set; }
+        [Required]
+        [EmailAddress]
+        public string email { get; set; }
+        [Required]
+        public DateTime birthday { get; set; }
     }
 }
