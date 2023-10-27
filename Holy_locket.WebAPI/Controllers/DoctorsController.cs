59e631fe2ced56665a3fecc9501dc9d6fcd86883
AleksTikhonov <122445936+rufijey@users.noreply.github.com>
2023-10-27 06:14:30 +0300

    doctors controller

---
+++ Holy_locket.WebAPI/Controllers/DoctorsController.cs
@@ -0,0 +1,118 @@
+﻿using Holy_locket.WebAPI.TestModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.IO;
+using System.Numerics;
+using System.Reflection.PortableExecutable;
+using System.Text.Json;
+
+namespace Holy_locket.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    //[Route("Doctors")]
+    public class DoctorsController : ControllerBase
+    {
+        List<Doctor> doctors = new List<Doctor>();
+
+
+        public DoctorsController()
+        {
+            try {
+                string fileName = "Doctors.json";
+
+
+                string jsonString;
+                using (StreamReader reader = new StreamReader(fileName))
+                {
+                    jsonString = reader.ReadToEnd();
+                }
+
+
+                doctors = JsonSerializer.Deserialize<List<Doctor>>(jsonString);
+            
+            } 
+            catch (Exception ex) {
+                return;
+            }
+            for (int id = 0; id < doctors.Count; id++) 
+            {
+                doctors[id].Id = id;
+            }
+
+
+
+        }
+
+        [HttpGet]
+        public IActionResult GetDoctors()
+        {
+            try {
+                string fileName = "Doctors.json";
+                string jsonString = JsonSerializer.Serialize(doctors);
+                using (StreamWriter writer = new StreamWriter(fileName))
+                {
+                    writer.Write(jsonString);
+                }
+                //ToDo: add getting data logic from DoctorsService
+
+                return Ok(doctors);
+            }
+            catch (Exception ex) {
+                return StatusCode(500, "An error occurred while processing your request.");
+            }
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetDoctorsId(int id)
+        {
+            try
+            {
+                //ToDo: add getting data logic from DoctorsService
+
+                return Ok(doctors[id]);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "An error occurred while processing your request.");
+            }
+        }
+        [HttpPost]
+        public IActionResult PostDoctors(Doctor doc)
+        {
+            doctors.Add(doc);
+            string fileName = "Doctors.json";
+            string jsonString = JsonSerializer.Serialize(doctors);
+            using (StreamWriter writer = new StreamWriter(fileName))
+            {
+                writer.Write(jsonString);
+            }
+            return Ok();
+        }
+        [HttpPut("{id}")]
+        public IActionResult PutDoctors(Doctor doc, int id)
+        {
+            doctors[id] = doc;
+            string fileName = "Doctors.json";
+            string jsonString = JsonSerializer.Serialize(doctors);
+            using (StreamWriter writer = new StreamWriter(fileName))
+            {
+                writer.Write(jsonString);
+            }
+            return Ok();
+        }
+        [HttpDelete("{id}")]
+        public IActionResult DeleteDoctors(int id)
+        {
+            doctors.RemoveAt(id);
+            string fileName = "Doctors.json";
+            string jsonString = JsonSerializer.Serialize(doctors);
+            using (StreamWriter writer = new StreamWriter(fileName))
+            {
+                writer.Write(jsonString);
+            }
+            return Ok();
+            
+        }
+    }
+}
---
+++ Holy_locket.WebAPI/TestModels/Doctor.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Holy_locket.WebAPI.TestModels
+{
+    public class Doctor
+    {
+       
+
+        public Doctor(int Id, string FirstName, string SecondName, string Description, bool Gender, string Phone)
+        {
+            this.Id = Id;
+            this.FirstName = FirstName;
+            this.SecondName = SecondName;
+            this.Description = Description;
+            this.Gender = Gender;
+            this.Phone = Phone;
+        }
+
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public string Description { get; set; }
+        public bool Gender { get; set; }
+        public string Phone { get; set; }
+        //public string Photo { get; set; }
+    }
+}
