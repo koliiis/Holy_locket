296f7fb0e37a9ab7d9e126c578a31714de2c7e70
Babenko Valera <11112004valerababenko@gmail.com>
2023-10-30 03:11:09 +0200

    create-rounting-and-navbar

---
+++ View/frontend/src/component/NavBar.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import './Navbar.css';
+
+const NavBar = () => {
+    const navigate = useNavigate();
+
+    return (
+        <div className="main">
+            <div className="logo">
+                <strong>HOLY Locket</strong>
+            </div>
+
+            <nav>
+                <ul className="choose">
+                    <li>
+                        <button className="btn" onClick={() => navigate("/registration")}>ГОЛОВНА</button>
+                    </li>
+
+                    <li>
+                        <button className="btn" onClick={() => navigate("/registration")}>Лікарі</button>
+                    </li>
+
+                    <li>
+                        <button className="btn" onClick={() => navigate("/registration")}>Мої записи</button>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    );
+};
+
+export default NavBar;
