33fd0965dd2dec98fff4df140e8e52f56499749e
Babenko Valera <11112004valerababenko@gmail.com>
2023-11-05 20:28:11 +0200

    update-cors

--- View/frontend/src/pages/doctors/doctors.js
+++ View/frontend/src/pages/doctors/doctors.js
@@ -17,86 +17,88 @@ function Doctors() {
             .catch(error => {
                 // Обработать ошибку, если запрос не удался
                 console.error("Ошибка при получении данных о врачах:", error);
+
             });
     }, []);
 
-    return (
-        <div className="master-doctor">
-            <div className="filters">
-                <p className="filt">Фільтр</p>
-                <ul className="many">
-                    <li>
-                        <button className='filt-btn'>Стать</button>
-                    </li>
-                    <li>
-                        <button className='filt-btn'>Стаж</button>
-                    </li>
-                    <li>
-                        <button className='filt-btn'>Спеціалізація</button>
-                    </li>
-                    <li>
-                        <button className='filt-btn'>Оцінка</button>
-                    </li>
-                </ul>
-                <div className="cards">
-                    <img className="ded" src="https://ggclinic.com.ua/wp-content/uploads/2022/06/doctor-full.jpeg"/>
-                    <h3 className="name">
-                        Степан Бандерович
-                        <p className="specail-card">
-                            Стоматолог
-                        </p>
-                    </h3>
-                    <h4 className='exp'>
-                        Працює понад 15 років
-                    </h4>
-                    <div className="info">
-                        Лікар Ванечка Петров - досвідчений лікар, який провчився на дистанційці і без проблем зможе
-                        вас вбити.
-                    </div>
-                    <button className="more_info" onClick={() => navigate("/appointment")}>Дізнатись</button>
-                </div>
-
-                <div className="cards">
-                    <img className="ded"  src="https://ggclinic.com.ua/wp-content/uploads/2022/06/doctor-full.jpeg"/>
-                    {doctors.map(doctor => (
-                        <div className="doctor-photo-cards" key={doctor.id}>
-
-                            <h3 className="name">
-                                {doctor.firstName}
-                                <p className="specail-card">
-                                    {doctor.secondName}
-                                </p>
-                            </h3>
-                            <h4 className='exp'>
-                                Працює понад {doctor.phone} років
-                            </h4>
-                            <div className="info">
-                                {doctor.description}
-                            </div>
-
+        return (
+            <div className="master-doctor">
+                <div className="filters">
+                    <p className="filt">Фільтр</p>
+                    <ul className="many">
+                        <li>
+                            <button className='filt-btn'>Стать</button>
+                        </li>
+                        <li>
+                            <button className='filt-btn'>Стаж</button>
+                        </li>
+                        <li>
+                            <button className='filt-btn'>Спеціалізація</button>
+                        </li>
+                        <li>
+                            <button className='filt-btn'>Оцінка</button>
+                        </li>
+                    </ul>
+                    <div className="cards">
+                        <img className="ded" src="https://ggclinic.com.ua/wp-content/uploads/2022/06/doctor-full.jpeg"/>
+                        <h3 className="name">
+                            Степан Бандерович
+                            <p className="specail-card">
+                                Стоматолог
+                            </p>
+                        </h3>
+                        <h4 className='exp'>
+                            Працює понад 15 років
+                        </h4>
+                        <div className="info">
+                            Лікар Ванечка Петров - досвідчений лікар, який провчився на дистанційці і без проблем зможе
+                            вас вбити.
                         </div>
-                    ))}
-                    <button className="more_info" onClick={() => navigate("/appointment")}>Дізнатись</button>
-                </div>
-                <div className="cards">
-                    <img className="ded" src="https://ggclinic.com.ua/wp-content/uploads/2022/06/doctor-full.jpeg"/>
-                    <h3 className="name">
-                        Степан Бандерович
-                        <p className="specail-card">
-                            Стоматолог
-                        </p>
-                    </h3>
-                    <h4 className='exp'>
-                        Працює понад 15 років
-                    </h4>
-                    <div className="info">
-                        Лікар Ванечка Петров - досвідчений лікар, який провчився на дистанційці і без проблем зможе
-                        вас вбити.
+                        <button className="more_info" onClick={() => navigate("/appointment")}>Дізнатись</button>
                     </div>
-                    <button className="more_info" onClick={() => navigate("/appointment")}>Дізнатись</button>
+
+                    <div className="cards">
+                        <img className="ded" src="https://ggclinic.com.ua/wp-content/uploads/2022/06/doctor-full.jpeg"/>
+                        {doctors.map(doctor => (
+                            <div className="doctor-photo-cards" key={doctor.id}>
+
+                                <h3 className="name">
+                                    {doctor.firstName}
+                                    <p className="specail-card">
+                                        {doctor.secondName}
+                                    </p>
+                                </h3>
+                                <h4 className='exp'>
+                                    Працює понад {doctor.phone} років
+                                </h4>
+                                <div className="info">
+                                    {doctor.description}
+                                </div>
+
+                            </div>
+                        ))}
+                        <button className="more_info" onClick={() => navigate("/appointment")}>Дізнатись</button>
+                    </div>
+                    <div className="cards">
+                        <img className="ded" src="https://ggclinic.com.ua/wp-content/uploads/2022/06/doctor-full.jpeg"/>
+                        <h3 className="name">
+                            Степан Бандерович
+                            <p className="specail-card">
+                                Стоматолог
+                            </p>
+                        </h3>
+                        <h4 className='exp'>
+                            Працює понад 15 років
+                        </h4>
+                        <div className="info">
+                            Лікар Ванечка Петров - досвідчений лікар, який провчився на дистанційці і без проблем зможе
+                            вас вбити.
+                        </div>
+                        <button className="more_info" onClick={() => navigate("/appointment")}>Дізнатись</button>
+                    </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+
 }
 export default Doctors;
