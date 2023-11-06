2a5e58f70656210e7221c1ca1bb94e2b0df239c6
Kostya Klopov <kklopov64@gmail.com>
2023-11-06 22:14:57 +0200

    merge from main

---
+++ Holy_locket.BLL/Services.Abstraction/IPatientService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Holy_locket.BLL.Services.Abstraction
+{
+    internal interface IPatientService : IDisposable
+    {
+        Task DeleteProject(int id);
+        Task DeleteProject(Patien project);
+        Task UpdateProject(ProjectDTO project);
+        Task CreateProject(ProjectDTO project);
+    }
+}
