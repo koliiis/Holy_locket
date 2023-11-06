a455957a9acab3c791f0f85cfbe61fe6b2d75980
Kostya Klopov <63253813+KazzModan@users.noreply.github.com>
2023-11-06 22:19:25 +0200

    up to date (#20)
    * added DTOs classes (#18)
    
    * connect-doctor's-page-and-registration-maket-post (#19)
    
    ---------
    
    Co-authored-by: babenko-v <146779776+babenko-v@users.noreply.github.com>

---
+++ Holy_locket.BLL/DTO/PatientDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Holy_locket.BLL.DTO
+{
+    public class PatientDTO
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public DateTime Birthday { get; set; }
+    }
+}
