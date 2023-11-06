51a7d20efd4a12ca7c1a204de3710c1bdb47697d
Kostya Klopov <63253813+KazzModan@users.noreply.github.com>
2023-11-06 12:34:24 +0200

    added DTOs classes (#18)

---
+++ Holy_locket.BLL/DTO/AppointmentDTO.cs
@@ -0,0 +1,19 @@
+﻿using Holy_locket.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Holy_locket.BLL.DTO
+{
+    internal class AppointmentDTO
+    {
+        public int Id { get; set; }
+        public DateTime Time { get; set; }
+        public int HospitalId { get; set; }
+        public int DoctorId { get; set; }
+        public int PatientId { get; set; }
+    }
+}
