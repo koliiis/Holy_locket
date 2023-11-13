0b6809fe887547a9cec938f9fc804257a2fcfbf7
Kostya Klopov <kklopov64@gmail.com>
2023-11-13 23:51:14 +0200

    create realisation

--- Holy_locket.BLL/Services.Abstraction/IAppointmentService.cs
+++ Holy_locket.BLL/Services.Abstraction/IAppointmentService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Holy_locket.BLL.DTO;
+using Holy_locket.DAL.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,12 @@ using System.Threading.Tasks;
 
 namespace Holy_locket.BLL.Services.Abstraction
 {
-    internal interface IAppointmentService
+    public interface IAppointmentService : IDisposable
     {
+        Task DeleteAppointment(int id);
+        Task UpdateAppointment(HospitalDTO hospital);
+        Task CreateAppointment(HospitalDTO hospital);
+        Task<HospitalDTO> GetAppointmentById(int id);
+        Task<ICollection<Hospital>> GetAll();
     }
 }
