5f3fa04d1b722c22f72aeef686ced5fa9c7cb8d4
Babenko Valera <11112004valerababenko@gmail.com>
2023-11-15 01:00:57 +0200

    update-3.0

--- View/frontend/src/component/modal-appointment.js
+++ View/frontend/src/component/modal-appointment.js
@@ -3,8 +3,8 @@ import './modal-appointment.css'
 function Modal_Appointment({active, setActive}) {
 
     return (
-        <div className={active ? "modal active" : "modal"} onClick={() => setActive(true)}>
-            <div className={active ? "modal_content active" : "modal_content"} onClick={e => e.stopPropagation()}>
+        <div className={active ? "modal acttive" : "modal"} onClick={() => setActive(true)}>
+            <div className={active ? "modal_content acttive" : "modal_content"} onClick={e => e.stopPropagation()}>
                 <p>shakalaka</p>
             </div>
         </div>
