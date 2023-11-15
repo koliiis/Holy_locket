a6abe2c8de34bb91a8f112be4f98847a3e309cb5
koliiis <123481146+koliiis@users.noreply.github.com>
2023-11-16 00:22:57 +0200

    Inf-appointments-from-back (#36)
    * from-back
    
    * correct
    
    * undone-but-nvm

--- View/frontend/src/pages/patientsappointments/patientsappointments.js
+++ View/frontend/src/pages/patientsappointments/patientsappointments.js
@@ -1,59 +1,59 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import "./patientsappointments.css";
 import { useNavigate } from 'react-router-dom';
 
 function Patientsappointments() {
     const navigate = useNavigate();
+    const [doctors, setDoctors] = useState([]);
+    const [specialities, setSpecialities] = useState({});
+
+    useEffect(() => {
+        axios.get('https://localhost:7172/api/Doctors')
+            .then(response => {
+                setDoctors(response.data);
+            })
+            .catch(error => {
+                console.error("Ошибка при получении данных о врачах:", error);
+            });
+
+            axios.get('https://localhost:7172/api/Speciality')
+            .then(response => {
+                const specialitiesData = {};
+                response.data.forEach(speciality => {
+                    specialitiesData[speciality.id] = speciality;
+                });
+                setSpecialities(specialitiesData);
+            })
+            .catch(error => {
+                console.error("Ошибка при получении данных о специальностях:", error);
+            });
+    }, []);
 
     return (
     <div className="body1">
-        <div className="div1">
-            <h2 className="h21">Костя Пивопив</h2>
-            <p className="p1">Акушер-гінеколог</p>
-            <h3 className="h31">Стан запису:</h3>
-            <div className="div21">ЗАКІНЧЕНО</div>
-            <button className="div3" onClick={() => navigate("/appointment")}>Записатися ще раз</button>
-            <h3 className="appdet">Деталі прийому:</h3>
-            <div className="div4">
-                <p className="p1">Дата: 6.11.2023</p>
-                <p className="p1">Час: 14:00</p>
-                <p className="p1">Адреса: вул. Михайла Брайчевського, 8</p>
-                <p className="p1">Кабінет: 418</p>
-            </div>
-        </div>
+
+        {doctors.map(doctor => (
+            <div className="div1">
+                <h2 className="h21">{doctor.firstName} {doctor.secondName}</h2>
+                <p className="p1">{specialities[doctor.specialityId]?.name || "Специальность не найдена"}</p>
+                <h3 className="h31">Стан запису:</h3>
+                <div className="div21">ЗАКІНЧЕНО</div>
+                <button className="div3" onClick={() => navigate("/doctors")}>Записатися ще раз</button>
+                <h3 className="appdet">Деталі прийому:</h3>
+                <div className="div4">
+                    <p className="p1">Дата: 6.11.2023</p>
+                    <p className="p1">Час: 14:00</p>
+                    <p className="p1">Кабінет: 418</p>
+                </div>
         <img className="div5" src="https://ggclinic.com.ua/wp-content/uploads/2022/06/doctor-full.jpeg"/>
-        <div className="div1">
-            <h2 className="h211">Валєра Непив</h2>
-            <p className="p1">Уролог</p>
-            <h3 className="h31">Стан запису:</h3>
-            <div className="div2">ПІДТВЕРДЖЕНО</div>
-            <button className="div3" onClick={() => navigate("/appointment")}>Записатися ще раз</button>
-            <h3 className="appdet">Деталі прийому:</h3>
-            <div className="div4">
-                <p className="p1">Дата: 10.11.2023</p>
-                <p className="p1">Час: 11:00</p>
-                <p className="p1">Адреса: вул. Академіка Янгеля, 7</p>
-                <p className="p1">Кабінет: 105</p>
-            </div>
         </div>
-        <img className="div5" src="https://ggclinic.com.ua/wp-content/uploads/2022/06/doctor-full.jpeg"/>
-        <div className="div1">
-            <h2 className="h211">Валєра Непив</h2>
-            <p className="p1">Уролог</p>
-            <h3 className="h31">Стан запису:</h3>
-            <div className="div2">ПІДТВЕРДЖЕНО</div>
-            <button className="div3" onClick={() => navigate("/appointment")}>Записатися ще раз</button>
-            <h3 className="appdet">Деталі прийому:</h3>
-            <div className="div4">
-                <p className="p1">Дата: 10.11.2023</p>
-                <p className="p1">Час: 11:00</p>
-                <p className="p1">Адреса: вул. Академіка Янгеля, 7</p>
-                <p className="p1">Кабінет: 105</p>
-            </div>
-        </div>
-        <img className="div5" src="https://ggclinic.com.ua/wp-content/uploads/2022/06/doctor-full.jpeg"/>
+        ))}
+
     </div>
-   );
+
+    
+    );
 }
 
 export default Patientsappointments;
