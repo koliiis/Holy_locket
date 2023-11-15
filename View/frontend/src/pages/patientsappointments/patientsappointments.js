ba73965ba0fc19df66982f52f49cd59d0b9acdd5
koliiis <123481146+koliiis@users.noreply.github.com>
2023-11-15 21:15:03 +0200

    correct

--- View/frontend/src/pages/patientsappointments/patientsappointments.js
+++ View/frontend/src/pages/patientsappointments/patientsappointments.js
@@ -6,6 +6,8 @@ import { useNavigate } from 'react-router-dom';
 function Patientsappointments() {
     const navigate = useNavigate();
     const [doctors, setDoctors] = useState([]);
+    const [specialities, setSpecialities] = useState({});
+
 
     useEffect(() => {
         axios.get('https://localhost:7172/api/Doctors')
@@ -15,6 +17,18 @@ function Patientsappointments() {
             .catch(error => {
                 console.error("Ошибка при получении данных о врачах:", error);
             });
+
+            axios.get('https://localhost:7172/api/Speciality')
+            .then(response => {
+                const specialitiesData = {};
+                response.data.forEach(speciality => {
+                    specialitiesData[speciality.id] = speciality;
+                });
+                setSpecialities(specialitiesData);
+            })
+            .catch(error => {
+                console.error("Ошибка при получении данных о специальностях:", error);
+            });
     }, []);
 
     return (
@@ -23,15 +37,14 @@ function Patientsappointments() {
         {doctors.map(doctor => (
             <div className="div1">
                 <h2 className="h21">{doctor.firstName} {doctor.secondName}</h2>
-                <p className="p1">{doctor.specialityId || "Специальность не найдена"}</p>
+                <p className="p1">{specialities[doctor.specialityId]?.name || "Специальность не найдена"}</p>
                 <h3 className="h31">Стан запису:</h3>
                 <div className="div21">ЗАКІНЧЕНО</div>
-                <button className="div3" onClick={() => navigate("/appointment")}>Записатися ще раз</button>
+                <button className="div3" onClick={() => navigate("/doctors")}>Записатися ще раз</button>
                 <h3 className="appdet">Деталі прийому:</h3>
                 <div className="div4">
                     <p className="p1">Дата: 6.11.2023</p>
                     <p className="p1">Час: 14:00</p>
-                    <p className="p1">Адреса: вул. Михайла Брайчевського, 8</p>
                     <p className="p1">Кабінет: 418</p>
                 </div>
         <img className="div5" src="https://ggclinic.com.ua/wp-content/uploads/2022/06/doctor-full.jpeg"/>
@@ -39,6 +52,8 @@ function Patientsappointments() {
         ))}
 
     </div>
+
+    
     );
 }
 
