76ad17b4fef7701d44d14e14051ba9d54ddbe671
koliiis <123481146+koliiis@users.noreply.github.com>
2023-11-16 11:54:28 +0200

    working21

--- View/frontend/src/pages/patientprofile/patientprofile.js
+++ View/frontend/src/pages/patientprofile/patientprofile.js
@@ -20,12 +20,12 @@ function Patientprofile() {
         <h2 className="h22">Костя Пивопив</h2> 
         <img className="img22" src={img2}/>
         <img className="img33" src={img3}/>
-        <div className='div11'><p className='p11'>Чоловік</p><img className="img55" src={img5}/></div>
+        <div className='div11'><p className='p11'>Чоловік</p><img className="img44" src={img5}/></div>
         <div className='div22'><p className='p11'>03.11.2004</p><img className="img44" src={img4}/></div>
         <div className='div22'><p className='p11'>yaloh@gmail.com</p><img className="img44" src={img6}/></div>
         <div className='div33'><p className='p11'>Україна, Київ</p><img className="img44" src={img7}/></div>
-        <div className='div44'><p className='p11'>+38(038)038-03-80</p><img className="img55" src={img8}/></div>
-        <div className='div44'><p className='p11'>1234567890</p><img className="img55" src={img9}/></div>
+        <div className='div44'><p className='p11'>+38(038)038-03-80</p><img className="img44" src={img8}/></div>
+        <div className='div44'><p className='p11'>1234567890</p><img className="img44" src={img9}/></div>
     </div>
    );
 }
