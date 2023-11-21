30a7f3ce20f65375a03e056e91598eba09dc2806
koliiis <123481146+koliiis@users.noreply.github.com>
2023-11-22 00:53:05 +0200

    test

--- View/frontend/src/pages/patientprofile/patientprofile.js
+++ View/frontend/src/pages/patientprofile/patientprofile.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import "./patientprofile.css";
-import { useNavigate } from 'react-router-dom';
 
 import img2 from './img2.png';
 import img3 from './img3.png';
@@ -12,19 +12,32 @@ import img8 from './img8.png';
 import img9 from './img9.png';
 
 function Patientprofile() {
-    const navigate = useNavigate();
+    const [InfoPage, setInfoPage] = useState([]);
+
+    useEffect(() => {
+        axios.get('https://localhost:7172/api/Patient/1')
+            .then(response => {
+                setInfoPage(response.data);
+            })
+            .catch(error => {
+                console.error("Помилка при отриманні даних:", error);
+            });
+    }, []);
+
     return (
     <div className="body11">
-        <img className="img11" src="https://ggclinic.com.ua/wp-content/uploads/2022/06/doctor-full.jpeg"/>
-        <h2 className="h22">Степан Дмитрук</h2>
-        <img className="img22" src={img2}/>
-        <img className="img33" src={img3}/>
-        <div className='div11'><p className='p11'>Чоловік</p><img className="img44" src={img5}/></div>
-        <div className='div22'><p className='p11'>03.11.2004</p><img className="img44" src={img4}/></div>
-        <div className='div22'><p className='p11'>holy_locket@gmail.com</p><img className="img44" src={img6}/></div>
-        <div className='div33'><p className='p11'>Україна, Київ</p><img className="img44" src={img7}/></div>
-        <div className='div44'><p className='p11'>+38(038)038-03-80</p><img className="img44" src={img8}/></div>
-        <div className='div44'><p className='p11'>1234567890</p><img className="img44" src={img9}/></div>
+        {InfoPage.map(infpage => (
+            <><img className="img11" src="https://ggclinic.com.ua/wp-content/uploads/2022/06/doctor-full.jpeg"/>
+            <h2 className="h22">{infpage.firstName} {infpage.secondName}</h2>
+            <img className="img22" src={img2}/>
+            <img className="img33" src={img3}/>
+            <div className='div11'><p className='p11'>Чоловік</p><img className="img44" src={img5}/></div>
+            <div className='div22'><p className='p11'>{infpage.birthday}</p><img className="img44" src={img4}/></div>
+            <div className='div22'><p className='p11'>{infpage.email}</p><img className="img44" src={img6}/></div>
+            <div className='div33'><p className='p11'>Україна, Київ</p><img className="img44" src={img7}/></div>
+            <div className='div44'><p className='p11'>{infpage.phone}</p><img className="img44" src={img8}/></div>
+            <div className='div44'><p className='p11'>1234567890</p><img className="img44" src={img9}/></div></>
+        ))}
     </div>
    );
 }
