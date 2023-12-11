1333408a2b381b3cefe172a64ff1ae349ebf9b29
AleksTikhonov <122445936+rufijey@users.noreply.github.com>
2023-12-11 15:49:46 +0200

    rating

---
+++ Holy_locket.DAL/Models/Rating.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Holy_locket.DAL.Models
+{
+    public class Rating
+    {
+        public int Id { get; set; }
+        public int DoctorId { get; set; }
+        public int PatientId { get; set; }
+        public int Rate { get; set; }
+        [NotMapped]
+        public Doctor Doctor { get; set; }
+        [NotMapped]
+        public Patient Patient { get; set; }
+    }
+}
