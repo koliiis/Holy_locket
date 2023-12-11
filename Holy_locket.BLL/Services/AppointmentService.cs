9b8d8f265a06ec428eb25cd35ea7f6169e20eed3
AleksTikhonov <122445936+rufijey@users.noreply.github.com>
2023-12-11 23:33:10 +0200

    1

--- Holy_locket.BLL/Services/AppointmentService.cs
+++ Holy_locket.BLL/Services/AppointmentService.cs
@@ -70,7 +70,7 @@ namespace Holy_locket.BLL.Services
             }
             foreach (var item in appointments)
             {
-                if (DateTime.Parse(item.Date) >= DateTime.Today.Date)
+                if (DateTime.Parse(item.Date) >= DateTime.Today.Date && item.Inactive == true)
                 {
                     counter = (DateTime.Parse(item.Date) - DateTime.Today).Days;
                     timeSlots[counter].Remove(item.Time);
