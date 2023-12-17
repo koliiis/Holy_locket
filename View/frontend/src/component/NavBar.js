171da824fb35153e1bf821885a0dda6b03b47ffa
Babenko Valera <11112004valerababenko@gmail.com>
2023-12-17 21:29:35 +0200

    new-land-page

--- View/frontend/src/component/NavBar.js
+++ View/frontend/src/component/NavBar.js
@@ -8,7 +8,7 @@ const NavBar = () => {
 
     return (
         <Navbar className="navchik" variant="dark" expand="lg">
-            <Navbar.Brand onClick={() => navigate("/landing")} className="logo">HOLY Locket</Navbar.Brand>
+            <Navbar.Brand onClick={() => navigate("/landing")} className="logo">Holy Locket</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav"/>
             <Navbar.Collapse id="basic-navbar-nav" className='custom-collapse'>
                 <Nav className="mr-auto align-content-center horizontal_nav">
