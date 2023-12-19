5a06ad9676d4671d7d77fb54734b0d4f3bf564f1
AleksTikhonov <122445936+rufijey@users.noreply.github.com>
2023-12-19 19:48:01 +0200

    4

--- Holy_locket.BLL/Services/DoctorService.cs
+++ Holy_locket.BLL/Services/DoctorService.cs
@@ -36,12 +36,7 @@ namespace Holy_locket.BLL.Services
         }
         public async Task<TokenInfoDTO> AddDoctor(DoctorDTO doctor)
         {
-            var speciality = await _specialityService.GetSpecialityById(doctor.SpecialityId);
-            doctor.SpecialityName = speciality.Name;
-            Console.WriteLine(doctor.SpecialityId + "  " + doctor.SpecialityName);
-            var d = _mapper.Map<Doctor>(doctor);
-            await _doctorRepository.Create(d).ConfigureAwait(false);
-            Console.WriteLine(d.SpecialityId);
+            await _doctorRepository.Create(_mapper.Map<Doctor>(doctor)).ConfigureAwait(false);
             var user = (await _doctorRepository.Get())
             .Where(d => d.Phone == doctor.Phone)
             .FirstOrDefault();
